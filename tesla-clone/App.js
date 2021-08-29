eabcba32030eeebe5cb01ce115b513a1d77a9c13
Will Davis <wdavis@shipt.com>
2021-08-29 12:42:11 -0500

    adding expo stuff and setting up components

---
+++ tesla-clone/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import CarItem from "./components/CarItem";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <CarItem />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fffa",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
