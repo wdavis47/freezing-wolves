adffb0f3640964a66cbcb035b5a741c0816ef235
Will Davis <wdavis@shipt.com>
2021-08-29 13:43:55 -0500

    add button and make it look nice and also add onpress functionality

--- tesla-clone/components/StyledButton/index.js
+++ tesla-clone/components/StyledButton/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import { View, Text, Pressable } from "react-native";
 import styles from "./styles";
 
-const StyledButton = (props) => {
+const StyledButton = ({ type, content, onPress }) => {
+  const backgroundColor = type === "primary" ? "#171A20CC" : "#FFFFFFA6";
+  const textColor = type === "primary" ? "#FFFFFF" : "171A20";
   return (
     <View style={styles.container}>
       <Pressable
-        style={styles.button}
-        onPress={() => {
-          console.warn("Hey There");
-        }}
+        style={[styles.button, { backgroundColor }]}
+        onPress={() => onPress()}
       >
-        <Text style={styles.text}>Custom Order</Text>
+        <Text style={[styles.text, { color: textColor }]}>{content}</Text>
       </Pressable>
     </View>
   );
