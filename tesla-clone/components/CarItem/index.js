3c765faf07411a62add883bf3a055c0d2d2b413c
Will Davis <wdavis@shipt.com>
2021-08-29 13:10:59 -0500

    breaking it up and adding a button component

--- tesla-clone/components/CarItem/index.js
+++ tesla-clone/components/CarItem/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, ImageBackground } from "react-native";
+import StyledButton from "../StyledButton";
 import styles from "./styles";
 
 const CarItem = (props) => {
@@ -13,6 +14,7 @@ const CarItem = (props) => {
         <Text style={styles.title}>Model S</Text>
         <Text style={styles.subtitle}>Starting at $69,000</Text>
       </View>
+      <StyledButton />
     </View>
   );
 };
---
+++ tesla-clone/components/StyledButton/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  button: {
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 20
+  },
+  container: {
+    width: "100%",
+    padding: 10
+  },
+  text: {
+    fontSize: 12,
+    fontWeight: "bold",
+    textTransform: "uppercase"
+  }
+});
+
+export default styles;
